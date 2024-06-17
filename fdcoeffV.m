eb3e20416c34f67260f7f10411b405fd61cd17f5
C0di1225 <cmaxwell@cub.uca.edu>
2024-06-16 20:01:22 -0500

    Add files via upload

---
+++ fdcoeffV.m
@@ -0,0 +1,19 @@
+﻿function c = fdcoeffV(k,xbar,x)
+    format long
+    n = length(x); % number of given function values
+    A = ones(n,n); % 
+    xrow = (x(:)-xbar)';
+    for i = 2:n
+        A(i,:) = (xrow.^(i-1))./factorial(i-1);
+    endfor
+    b = zeros(n,1);
+    b(k+1) = 1;
+    c = A\b;
+    c = c';
+endfunction
+
+% kth order approximation scheme given n points
+% derivative is given by
+% c*u(x) where c and x are vectors like
+% for example
+% u' approx c1*u1+c2*u2
